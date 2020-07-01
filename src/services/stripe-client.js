e2ad7123f27b47dfa7de1bff2bd65a8a54d5d53b
ljorgens <leejorgensen22@gmail.com>
2020-07-01 14:18:48 -0700

    change variable names

--- src/services/stripe-client.js
+++ src/services/stripe-client.js
@@ -1,5 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js'
 
-const stripe = async () => loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+const stripe = async () => loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
 
 export default stripe
